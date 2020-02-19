4fddec08633bbbf5c12d877dd02a8b32c9ac2907
github20 <github20@users.noreply.github.com>
2020-02-19 18:38:45 -0500

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -5,4 +5,26 @@ while (arr.length < 101) {
   var r = Math.floor(Math.random() * 100) + 1;
   if (arr.indexOf(r) === -1) arr.push(r);
 }
+
+  function bubbleSort(A){
+  var switched,
+      len = arr.length;
+  if(len === 1) return;
+
+  do {
+    switched = false;
+    for(var i=1;i<len;i++) {
+      if(A[i-1] > A[i]) {   
+        var b = A[i];
+        A[i] = A[i-1];
+        A[i-1] = b;
+        switched = true;
+      }   
+    }
+
+  }
+  while(switched)  
+}
+
+bubbleSort(arr);
 console.log(arr);
