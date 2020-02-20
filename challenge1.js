94e452c24d3dd012924bdd4e0b6093d2ba2ea6fa
github20 <github20@users.noreply.github.com>
2020-02-19 19:21:50 -0500

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,7 +1,7 @@
 // Basic random number generator and bubble sort.
 // Instructions: Run the code.
 var arr = [];
-while (arr.length < 101) {
+while (arr.length < 100) {
   var r = Math.floor(Math.random() * 100) + 1;
   if (arr.indexOf(r) === -1) arr.push(r);
 };
